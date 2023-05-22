1ac9b5a9117cb293ede841472b986066b5641635
Hi. I'm Neo <jthug@naver.com>
2023-05-22 09:34:55 +0900

    init: set recoil root set recoil root for global states

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {
   Route,
   Routes
 } from "react-router-dom"
+import {
+  RecoilRoot
+} from "recoil"
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 import Header from "./components/templates/Header/Header";
@@ -12,15 +15,17 @@ import Footer from "./components/templates/Footer/Footer";
 export default function App() {
 
   return (
-    <BrowserRouter>
-      <Header />
-      <Body>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </Body>
-      <Footer />
-    </BrowserRouter>
-  );
+    <RecoilRoot>
+      <BrowserRouter>
+        <Header />
+        <Body>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </Body>
+        <Footer />
+      </BrowserRouter>
+    </RecoilRoot>
+  )
 }
