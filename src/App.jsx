86a8f800c907ef39f3f8136cf0a3e465c07bd63a
Hi. I'm Neo <jthug@naver.com>
2023-05-22 09:32:20 +0900

    init: basic route Header, Body, Footer placed Home, NotFound routed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom"
+import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound/NotFound";
+import Header from "./components/templates/Header/Header";
+import Body from "./components/templates/Body/Body";
+import Footer from "./components/templates/Footer/Footer";
+
 export default function App() {
 
   return (
-    <div className="App">
-      hi react!
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Body>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Body>
+      <Footer />
+    </BrowserRouter>
   );
 }
