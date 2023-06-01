316f363f207c5e29038898e9533b0b855a95cf88
Hi. I'm Neo <jthug@naver.com>
2023-06-01 15:02:18 +0900

    !Hotfix: fix state "currentCategory" change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+import { useSetRecoilState } from "recoil"
+import { currentCategorySelector } from "../../globalStates/selectors"
+
 export default function Home() {
+
+  const setCurrentCategory = useSetRecoilState(currentCategorySelector)
+  
+  useEffect(() => {
+    setCurrentCategory(null)
+  }, [setCurrentCategory])
+
   return (
     <h1>
       Home
